a98b0e4954d9f4e1804497481859b0152d110dce
yaswanthyash6453 <ykaribugatha@gmaile.com>
2025-11-19 09:38:31 +0530

    merged

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -46,7 +46,7 @@ export default function Contact() {
 
       {/* WhatsApp Floating Button */}
       <a
-        href="https://wa.me/918919275680"
+        href="https://wa.me/919553727352"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-xl hover:scale-110 transition z-50 animate-bounce"
